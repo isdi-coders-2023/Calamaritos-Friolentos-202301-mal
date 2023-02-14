f1c4b8f5620a8450ad302155ef10652fc8861f3d
luis <luisrym@gmail.com>
2023-02-14 18:26:17 +0100

    try my provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { getSolarSystemPlanets } from "./api/planet.data";
 import "./App.css";
+import PlanetContextProvider from "./store/contexts/PlanetContext.provider";
 
 function App() {
   getSolarSystemPlanets();
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <PlanetContextProvider>
+        <h1>hola</h1>
+        <h1>hola</h1>
+      </PlanetContextProvider>
+    </div>
+  );
 }
 
 export default App;
