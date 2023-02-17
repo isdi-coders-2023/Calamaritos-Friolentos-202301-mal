66c6d7a056549e8855fd1d8c89bd090b24ae6132
`thais` <`tjlannes@gmail.com`>
2023-02-17 12:03:35 +0100

    creation of the breadcrumbs component

---
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Breadcrumbs.css";
+
+interface BreadcrumbItem {
+  name: string;
+  link: string;
+}
+
+interface BreadcrumbProps {
+  items: BreadcrumbItem[];
+}
+
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ items }) => {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        {items.map((item, index) => (
+          <li key={index} className="breadcrumb-item">
+            {index === items.length - 1 ? (
+              <span>{item.name}</span>
+            ) : (
+              <a href={item.link}>{item.name}</a>
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
