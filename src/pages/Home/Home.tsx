3a917fe79b2fe52c26773c7418c8645c419e1d8b
LorenaAnayaDiaz <lorena.anaya.diaz@gmail.com>
2023-02-18 21:18:41 +0100

    change links in home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,9 @@
-
 import Pagination from "../../components/Pagination/Pagination";
 import PlanetsList from "../../components/PlanetsList/PlanetsList";
 import "./Home.css";
 import Breadcrumb from "../../components/Breadcrumbs/Breadcrumbs";
 
-const breadcrumbItems = [{ name: "Home", link: "/Home" }];
+const breadcrumbItems = [{ name: "Home", link: "/Main" }];
 
 export const Home = () => {
   return (
