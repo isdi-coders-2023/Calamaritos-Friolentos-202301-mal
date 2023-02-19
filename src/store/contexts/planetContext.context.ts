742fdc7549eec778795437664b221ae43ef905fd
`thais` <`tjlannes@gmail.com`>
2023-02-19 18:25:31 +0100

    add planet property in the context

--- src/store/contexts/planetContext.context.ts
+++ src/store/contexts/planetContext.context.ts
@@ -8,7 +8,21 @@ interface AppContextProps {
 }
 
 const AppContext = createContext<AppContextProps>({
-  state: { planets: [], page: 1 },
+  state: {
+    planets: [],
+    page: 1,
+    planet: {
+      name: "",
+      mass: 0,
+      radius: 0,
+      period: 0,
+      semi_major_axis: 0,
+      temperature: 0,
+      distance_light_year: 0,
+      host_star_mass: 0,
+      host_star_temperature: 0,
+    },
+  },
   dispatch: () => {},
 });
 
