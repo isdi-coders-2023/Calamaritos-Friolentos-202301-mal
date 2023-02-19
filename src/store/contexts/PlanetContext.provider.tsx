babdc2b75ee7c1c26e2bb0cde213efbc6c1215cf
`thais` <`tjlannes@gmail.com`>
2023-02-19 18:22:31 +0100

    add planet property

--- src/store/contexts/PlanetContext.provider.tsx
+++ src/store/contexts/PlanetContext.provider.tsx
@@ -12,7 +12,21 @@ interface AppContextProviderProps {
 const AppContextProvider: FC<AppContextProviderProps> = ({ children }) => {
   const [state, dispatch] = useReducer<Reducer<AppState, AppActions>>(
     planetReducer,
-    { planets: [], page: 1 }
+    {
+      planets: [],
+      page: 1,
+      planet: {
+        name: "",
+        mass: 0,
+        radius: 0,
+        period: 0,
+        semi_major_axis: 0,
+        temperature: 0,
+        distance_light_year: 0,
+        host_star_mass: 0,
+        host_star_temperature: 0,
+      },
+    }
   );
 
   return (
